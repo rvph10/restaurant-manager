794efc6ed9f29f19d32b6e988cbe22e70bf5c0f4
rvph10 <rgenu10@gmail.com>
2024-12-02 18:41:00 +0100

    feat(product): added methods to work with the db, added menu management in db,

--- backend/src/interfaces/product.interface.ts
+++ backend/src/interfaces/product.interface.ts
@@ -1,6 +1,7 @@
-import { MeasurementUnit, IngredientCategory } from '@prisma/client';
+import { MeasurementUnit, IngredientCategory, Category } from '@prisma/client';
 
 export interface CreateIngredientInput {
+  user: string;
   name: string;
   description?: string;
   stock: number;
@@ -13,4 +14,37 @@ export interface CreateIngredientInput {
   extraPrice?: number;
   supplierId?: string;
   price: number;
+}
+
+export interface CreateCategoryInput {
+  user: string;
+  name: string;
+  description?: string;
+  displayOrder: number;
+  isActive: boolean | true;
+  parentId?: string;
+}
+
+export interface CreateProductInput {
+  user: string;
+  name: string;
+  description?: string;
+  price: number;
+  unit: MeasurementUnit;
+  categoryId: string;
+  image?: string;
+  isAvailable: boolean | true;
+  preparationTime: number;
+  allergens: string[] | [];
+  freeExtras: number;
+  freeExtrasCategory: IngredientCategory | null;
+  ingredients: string[];
+}
+
+export interface CreateSupplierInput {
+    user: string;
+  name: string;
+  email?: string;
+  phone: string;
+  address?: string;
 }
--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -6,9 +6,15 @@ import {
   Ingredient,
   MeasurementUnit,
   IngredientCategory,
+  Supplier,
 } from '@prisma/client';
 import { logger } from '../lib/logging/logger';
-import { CreateIngredientInput } from '../interfaces/product.interface';
+import {
+  CreateCategoryInput,
+  CreateIngredientInput,
+  CreateProductInput,
+  CreateSupplierInput,
+} from '../interfaces/product.interface';
 import { auditLog } from '../lib/logging/logger';
 import { Decimal } from '@prisma/client/runtime/library';
 
@@ -38,18 +44,35 @@ export class ProductService {
     throw new Error('An unexpected error occurred');
   }
 
-  async checkSupplierExists(supplierId: string): Promise<boolean> {
+  async checkProductExists(params : {id?: string, name?: string}): Promise<boolean> {
     try {
-      if (!supplierId) {
-        throw new Error('Supplier ID must be provided');
+      if (!params.id && !params.name) {
+        throw new Error('At least one parameter (id or name) must be provided');
       }
-      const supplier = await prisma.supplier.findUnique({
-        where: { id: supplierId },
+
+      const whereClause: Prisma.ProductWhereInput = {
+        OR: [],
+      };
+
+      if (params.id) {
+        whereClause.OR!.push({ id: params.id });
+      }
+      if (params.name) {
+        whereClause.OR!.push({
+          name: {
+            equals: params.name,
+            mode: 'insensitive', // Case insensitive search
+          },
+        });
+      }
+
+      const product = await prisma.product.findFirst({
+        where: whereClause,
       });
 
-      return !!supplier;
+      return !!product;
     } catch (error) {
-      return this.handleServiceError(error, 'checkSupplierExists');
+      return this.handleServiceError(error, 'checkProductExists');
     }
   }
 
@@ -85,6 +108,78 @@ export class ProductService {
     }
   }
 
+  async checkCategoryExists(params: { id?: string; name?: string }): Promise<boolean> {
+    try {
+      if (!params.id && !params.name) {
+        throw new Error('At least one parameter (id or name) must be provided');
+      }
+
+      const whereClause: Prisma.CategoryWhereInput = {
+        OR: [],
+      };
+
+      if (params.id) {
+        whereClause.OR!.push({ id: params.id });
+      }
+      if (params.name) {
+        whereClause.OR!.push({
+          name: {
+            equals: params.name,
+            mode: 'insensitive', // Case insensitive search
+          },
+        });
+      }
+
+      const category = await prisma.category.findFirst({
+        where: whereClause,
+      });
+
+      return !!category;
+    } catch (error) {
+      return this.handleServiceError(error, 'checkCategoryExists');
+    }
+  }
+
+  async checkSupplierExists(params : { id?: string; name?: string, phone?: string}): Promise<boolean> {
+    try {
+      if (!params.id && !params.name && !params.phone) {
+        throw new Error('At least one parameter (id, name or phone) must be provided');
+      }
+
+      const whereClause: Prisma.SupplierWhereInput = {
+        OR: [],
+      };
+
+      if (params.id) {
+        whereClause.OR!.push({ id: params.id });
+      }
+      if (params.name) {
+        whereClause.OR!.push({
+          name: {
+            equals: params.name,
+            mode: 'insensitive', // Case insensitive search
+          },
+        });
+      }
+      if (params.phone) {
+        whereClause.OR!.push({
+          phone: {
+            equals: params.phone,
+            mode: 'insensitive', // Case insensitive search
+          },
+        });
+      }
+
+      const supplier = await prisma.supplier.findFirst({
+        where: whereClause,
+      });
+
+      return !!supplier;
+    } catch (error) {
+      return this.handleServiceError(error, 'checkSupplierExists');
+    }
+  }
+
   async createIngredient(data: CreateIngredientInput): Promise<Ingredient> {
     try {
       if (data.cost < 0) {
@@ -117,7 +212,7 @@ export class ProductService {
         throw new Error('Extra price must be a positive number');
       }
 
-      if (data.supplierId && !(await this.checkSupplierExists(data.supplierId))) {
+      if (data.supplierId && !(await this.checkSupplierExists({ id: data.supplierId }))) {
         throw new Error('Supplier with this ID does not exist');
       }
 
@@ -147,7 +242,7 @@ export class ProductService {
           entityName: data.name,
           entityID: ingredient.id,
         },
-        ingredient.id
+        data.user
       );
 
       return ingredient;
@@ -188,7 +283,7 @@ export class ProductService {
         throw new Error('Extra price must be a positive number');
       }
 
-      if (data.supplierId && !(await this.checkSupplierExists(data.supplierId))) {
+      if (data.supplierId && !(await this.checkSupplierExists({ id: data.supplierId }))) {
         throw new Error('Supplier with this ID does not exist');
       }
 
@@ -215,12 +310,431 @@ export class ProductService {
           entityName: data.name,
           entityID: ingredient,
         },
-        ingredient.id
+        data.user
       );
 
       return ingredient;
     } catch (error) {
       return this.handleServiceError(error, 'updateIngredient');
+    }
+  }
+
+  async deleteIngredient(id: string): Promise<void> {
+    try {
+      if (!(await this.checkIngredientExists({ id }))) {
+        throw new Error('Ingredient with this ID does not exist');
+      }
+      const ingredient = await prisma.ingredient.delete({
+        where: { id },
+      });
+
+      auditLog(
+        'DELETE',
+        {
+          entityName: ingredient.name,
+          entityID: ingredient.id,
+        },
+        ingredient.id
+      );
+    } catch (error) {
+      return this.handleServiceError(error, 'deleteIngredient');
+    }
+  }
+
+  async getIngredients(): Promise<Ingredient[]> {
+    try {
+      return await prisma.ingredient.findMany();
+    } catch (error) {
+      return this.handleServiceError(error, 'getIngredients');
+    }
+  }
+
+  async getIngredient(id: string): Promise<Ingredient | null> {
+    try {
+      if (!id) {
+        throw new Error('ID must be provided');
+      }
+      const ingredient = await prisma.ingredient.findUnique({
+        where: { id },
+      });
+      if (!ingredient) {
+        throw new Error('Ingredient not found');
+      }
+      return ingredient;
+    } catch (error) {
+      return this.handleServiceError(error, 'getIngredient');
+    }
+  }
+
+  async getCategories(): Promise<Category[]> {
+    try {
+      return await prisma.category.findMany();
+    } catch (error) {
+      return this.handleServiceError(error, 'getCategories');
+    }
+  }
+
+  async getCategory(id: string): Promise<Category | null> {
+    try {
+      if (!id) {
+        throw new Error('ID must be provided');
+      }
+      const category = await prisma.category.findUnique({
+        where: { id },
+      });
+      if (!category) {
+        throw new Error('Category not found');
+      }
+      return category;
+    } catch (error) {
+      return this.handleServiceError(error, 'getCategory');
+    }
+  }
+
+  async createCategory(data: CreateCategoryInput, newParentCategory: boolean): Promise<Category> {
+    try {
+      if (data.displayOrder < 0) {
+        throw new Error('Display order must be a positive number');
+      }
+      if (data.description && data.description.length > 62) {
+        throw new Error('Description must be at most 255 characters');
+      }
+      if (await this.checkCategoryExists({ name: data.name })) {
+        throw new Error('Category with this name already exists');
+      }
+      let nextCategory = { displayOrder: 0 };
+      if (newParentCategory) {
+        const foundCategory = await prisma.category.findFirst({
+          orderBy: { displayOrder: 'desc' },
+        });
+        nextCategory = foundCategory ? foundCategory : nextCategory;
+      }
+      const category = await prisma.category.create({
+        data: {
+          name: data.name,
+          description: data.description,
+          displayOrder: newParentCategory
+            ? (nextCategory?.displayOrder || 0) + 1
+            : data.displayOrder,
+          isActive: data.isActive,
+          parentId: data.parentId,
+        },
+      });
+      auditLog(
+        'CREATE',
+        {
+          entityName: data.name,
+          entityID: category.id,
+        },
+        data.user
+      );
+      return category;
+    } catch (error) {
+      return this.handleServiceError(error, 'createCategory');
+    }
+  }
+
+  async updateCategory(id: string, data: CreateCategoryInput): Promise<Category> {
+    try {
+      if (data.displayOrder < 0) {
+        throw new Error('Display order must be a positive number');
+      }
+      if (data.description && data.description.length > 62) {
+        throw new Error('Description must be at most 255 characters');
+      }
+      if (await this.checkCategoryExists({ name: data.name })) {
+        throw new Error('Category with this name already exists');
+      }
+      const category = await prisma.category.update({
+        where: { id },
+        data: {
+          name: data.name,
+          description: data.description,
+          displayOrder: data.displayOrder,
+          isActive: data.isActive,
+          parentId: data.parentId,
+        },
+      });
+      auditLog(
+        'UPDATE',
+        {
+          entityName: data.name,
+          entityID: category,
+        },
+        data.user
+      );
+      return category;
+    } catch (error) {
+      return this.handleServiceError(error, 'updateCategory');
+    }
+  }
+
+  async deleteCategory(id: string, user: string): Promise<void> {
+    try {
+      if (!(await this.checkCategoryExists({ id }))) {
+        throw new Error('Category with this ID does not exist');
+      }
+      const category = await prisma.category.delete({
+        where: { id },
+      });
+      auditLog(
+        'DELETE',
+        {
+          entityName: category.name,
+          entityID: category.id,
+        },
+        user
+      );
+    } catch (error) {
+      return this.handleServiceError(error, 'deleteCategory');
+    }
+  }
+
+  async getIngredientsByCategory(category: string): Promise<Ingredient[]> {
+    try {
+      if (!category) {
+        throw new Error('Category must be provided');
+      }
+      if (!Object.values(IngredientCategory).includes(category as IngredientCategory)) {
+        throw new Error('Invalid category');
+      }
+      return await prisma.ingredient.findMany({
+        where: { category: category as IngredientCategory },
+      });
+    } catch (error) {
+      return this.handleServiceError(error, 'getIngredientsByCategory');
+    }
+  }
+
+  async getIngredientsBySupplier(supplierId: string): Promise<Ingredient[]> {
+    try {
+      if (!supplierId) {
+        throw new Error('Supplier ID must be provided');
+      }
+      if (!(await this.checkSupplierExists({ id: supplierId }))) {
+        throw new Error('Supplier with this ID does not exist');
+      }
+      return await prisma.ingredient.findMany({
+        where: { supplierId },
+      });
+    } catch (error) {
+      return this.handleServiceError(error, 'getIngredientsBySupplier');
+    }
+  }
+
+  async createProduct(data: CreateProductInput): Promise<Product> {
+    try {
+      if (data.price < 0) {
+        throw new Error('Price must be a positive number');
+      }
+      if (data.freeExtras > 0 && !data.freeExtrasCategory) {
+        throw new Error('Free extras category must be provided');
+      }
+      if (data.preparationTime < 0) {
+        throw new Error('Preparation time must be a positive number');
+      }
+      if (await this.checkCategoryExists({ name: data.name })) {
+        throw new Error('Category with this name already exists');
+      }
+      if (await this.checkIngredientExists({ name: data.name })) {
+        throw new Error('Ingredient with this name already exists');
+      }
+      const product = await prisma.product.create({
+        data: {
+          name: data.name,
+          description: data.description || null,
+          price: new Decimal(data.price),
+          categoryId: data.categoryId,
+          image: data.image || null,
+          isAvailable: data.isAvailable,
+          preparationTime: data.preparationTime || 15,
+          allergens: data.allergens || [],
+          freeExtras: data.freeExtras || 0,
+          freeExtraItems: Array.isArray(data.freeExtrasCategory) ? data.freeExtrasCategory : [],
+          ingredients: {
+            connect: data.ingredients.map((ingredient) => ({ id: ingredient })),
+          },
+        },
+      });
+      auditLog(
+        'CREATE',
+        {
+          entityName: data.name,
+          entityID: product.id,
+        },
+        data.user
+      );
+      return product;
+    } catch (error) {
+      return this.handleServiceError(error, 'createProduct');
+    }
+  }
+
+  async updateProduct(id: string, data: CreateProductInput): Promise<Product> {
+    try {
+      if (data.price < 0) {
+        throw new Error('Price must be a positive number');
+      }
+      if (data.freeExtras > 0 && !data.freeExtrasCategory) {
+        throw new Error('Free extras category must be provided');
+      }
+      if (data.preparationTime < 0) {
+        throw new Error('Preparation time must be a positive number');
+      }
+      if (await this.checkCategoryExists({ name: data.name })) {
+        throw new Error('Category with this name already exists');
+      }
+      if (await this.checkIngredientExists({ name: data.name })) {
+        throw new Error('Ingredient with this name already exists');
+      }
+      const product = await prisma.product.update({
+        where: { id },
+        data: {
+          name: data.name,
+          description: data.description || null,
+          price: new Decimal(data.price),
+          categoryId: data.categoryId,
+          image: data.image || null,
+          isAvailable: data.isAvailable,
+          preparationTime: data.preparationTime || 15,
+          allergens: data.allergens || [],
+          freeExtras: data.freeExtras || 0,
+          freeExtraItems: Array.isArray(data.freeExtrasCategory) ? data.freeExtrasCategory : [],
+          ingredients: {
+            set: data.ingredients.map((ingredient) => ({ id: ingredient })),
+          },
+        },
+      });
+      auditLog(
+        'UPDATE',
+        {
+          entityName: data.name,
+          entityID: product,
+        },
+        data.user
+      );
+      return product;
+    } catch (error) {
+      return this.handleServiceError(error, 'updateProduct');
+    }
+  }
+
+  async deleteProduct(id: string, user: string): Promise<void> {
+    try {
+      if (!(await this.checkIngredientExists({ id }))) {
+        throw new Error('Ingredient with this ID does not exist');
+      }
+      const product = await prisma.product.delete({
+        where: { id },
+      });
+      auditLog(
+        'DELETE',
+        {
+          entityName: product.name,
+          entityID: product.id,
+        },
+        user
+      );
+    } catch (error) {
+      return this.handleServiceError(error, 'deleteProduct');
+    }
+  }
+
+  async getProducts(): Promise<Product[]> {
+    try {
+      return await prisma.product.findMany();
+    } catch (error) {
+      return this.handleServiceError(error, 'getProducts');
+    }
+  }
+
+  async getProduct(id: string): Promise<Product | null> {
+    try {
+      if (!id) {
+        throw new Error('ID must be provided');
+      }
+      const product = await prisma.product.findUnique({
+        where: { id },
+      });
+      if (!product) {
+        throw new Error('Product not found');
+      }
+      return product;
+    } catch (error) {
+      return this.handleServiceError(error, 'getProduct');
+    }
+  }
+
+  async getProductsByCategory(category: string): Promise<Product[]> {
+    try {
+      if (!category) {
+        throw new Error('Category must be provided');
+      }
+      return await prisma.product.findMany({
+        where: { categoryId: category },
+      });
+    } catch (error) {
+      return this.handleServiceError(error, 'getProductsByCategory');
+    }
+  }
+
+  async createSupplier(data: CreateSupplierInput): Promise<Supplier> {
+    try {
+        if (!data.name) {
+            throw new Error('Name must be provided');
+        }
+        if (!data.phone) {
+            throw new Error('Phone must be provided');
+        }
+
+        if (await this.checkSupplierExists({ name: data.name, phone: data.phone })) {
+            throw new Error('Supplier with this name/phone already exists');
+        }
+      const supplier = await prisma.supplier.create({
+        data: {
+          name: data.name,
+          email: data.email || null,
+          phone: data.phone,
+          address: data.address || null,
+        },
+      });
+      auditLog(
+        'CREATE',
+        {
+          entityName: data.name,
+          entityID: supplier.id,
+        },
+        data.user
+      );
+      return supplier;
+    } catch (error) {
+      return this.handleServiceError(error, 'createSupplier');
+    }
+  }
+
+  async updateSupplier(id: string, data: CreateSupplierInput): Promise<Supplier> {
+    try {
+      
+      const supplier = await prisma.supplier.update({
+        where: { id },
+        data: {
+          name: data.name,
+          email: data.email || null,
+          phone: data.phone,
+          address: data.address || null,
+        },
+      });
+        auditLog(
+            'UPDATE',
+            {
+            entityName: data.name,
+            entityID: supplier,
+            },
+            data.user
+        );
+      return supplier;
+    } catch (error) {
+      return this.handleServiceError(error, 'updateSupplier');
     }
   }
 }
