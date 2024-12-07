b80c50d7c8c331ff49a2768446c67058d25728b0
rvph10 <rgenu10@gmail.com>
2024-12-07 19:27:39 +0100

    format

--- backend/src/interfaces/order.interface.ts
+++ backend/src/interfaces/order.interface.ts
@@ -1,26 +1,26 @@
-import { OrderItemStatus, OrderStatus, OrderType } from "@prisma/client";
+import { OrderItemStatus, OrderStatus, OrderType } from '@prisma/client';
 
 export interface OrderDataInput {
-    orderNumber: string;
-    customerId: string;
-    type: OrderType;
-    status: OrderStatus;
-    items: OrderItemDataInput;
-    totalAmount: number;
-    tax: number;
-    discount: number;
-    deliveryFee?: number;
-    tableId: string;
-    notes?: string;
+  orderNumber: string;
+  customerId: string;
+  type: OrderType;
+  status: OrderStatus;
+  items: OrderItemDataInput;
+  totalAmount: number;
+  tax: number;
+  discount: number;
+  deliveryFee?: number;
+  tableId: string;
+  notes?: string;
 }
 
 export interface OrderItemDataInput {
-    orderId: string;
-    productId: string;
-    quantity: number;
-    unitPrice: number;
-    modifications: JSON;
-    extraPrice: number;
-    specialRequest: string;
-    status: OrderItemStatus;
+  orderId: string;
+  productId: string;
+  quantity: number;
+  unitPrice: number;
+  modifications: JSON;
+  extraPrice: number;
+  specialRequest: string;
+  status: OrderItemStatus;
 }
